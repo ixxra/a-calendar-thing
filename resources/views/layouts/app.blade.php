c58ff912bca720b019f72babec056c34ca1c6f96
ixxra <garciamx@gmail.com>
2017-09-08 02:57:34 -0500

    disabled user config

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,18 +58,18 @@
                                             Calendario
                                         </a>
                                     </li>
-                                    <li>
-                                        <a href="#">
-                                            Cuenta de usuario
-                                        </a>
-                                    </li>
+                                    <!-- <li>
+                                         <a href="#">
+                                         Cuenta de usuario
+                                         </a>
+                                         </li> -->
                                     <li>
                                         <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
+                                           onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                             Salir
                                         </a>
-
+                                        
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                             {{ csrf_field() }}
                                         </form>
